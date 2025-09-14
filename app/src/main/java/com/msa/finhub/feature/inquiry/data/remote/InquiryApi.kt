39fa17349db754fcbf03afae2c2ef41dc99f8112
SOFTWARE-SV\zar <zartablet2022@gmail.com>
2025-09-14 16:32:03 +0330

    query InquiryApiImpl

--- app/src/main/java/com/msa/finhub/feature/inquiry/data/remote/InquiryApi.kt
+++ app/src/main/java/com/msa/finhub/feature/inquiry/data/remote/InquiryApi.kt
@@ -2,6 +2,7 @@ package com.msa.finhub.feature.inquiry.data.remote
 
 import com.msa.finhub.core.network.BaseRepository
 import com.msa.finhub.core.network.error.NetworkResult
+import io.ktor.http.encodeURLQueryComponent
 import kotlinx.serialization.json.JsonObject
 import timber.log.Timber
 /**
@@ -22,14 +23,14 @@ class InquiryApiImpl : BaseRepository(), InquiryApi {
         path: String,
         params: Map<String, String>
     ): NetworkResult<JsonObject> {
-        Timber.d("API fetch path=${'$'}path params=${'$'}params")
+        Timber.d("API fetch path=$path params=$params")
         val query = params.entries.joinToString("&") { (k, v) ->
-            "${'$'}k=${'$'}{v.encodeURLQueryComponent()}"
+            "$k=${v.encodeURLQueryComponent()}"
         }
-        val url = if (query.isNotEmpty()) "${'$'}path?${'$'}query" else path
-        Timber.d("API final url=${'$'}url")
+        val url = if (query.isNotEmpty()) "$path?$query" else path
+        Timber.d("API final url=$url")
         val result: NetworkResult<JsonObject> = get(url)
-        Timber.d("API result=${'$'}result")
+        Timber.d("API result=$result")
         return result
     }
 }
